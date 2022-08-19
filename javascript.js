b6a9c0cda6b18b8d171573d041bfd1191099ac4f
Mateusz-Platek <m.platek1705@gmail.com>
2022-08-19 14:04:51 +0200

    Add option of changing grid and changing color of squares

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function createSquares(elements, box) {
     const square = document.createElement('div');
-    square.style.border = '2px solid black';
-
+    square.classList.add('square');
+    square.style.border = '1px solid black';
     for(let i = 0; i < elements * elements; i++) {
         box.appendChild(square.cloneNode(true));
     }
@@ -10,3 +10,32 @@ function createSquares(elements, box) {
 const container = document.querySelector('.container')
 
 createSquares(16, container);
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "yellow";
+        });
+    }
+)
+
+const size = document.querySelector('.size');
+
+size.addEventListener('click', () => {
+    let rowsAndCols = prompt('Type size lower or equal 100:');
+    if(rowsAndCols > 100) {
+        return;
+    }
+    container.textContent = '';
+    container.style.gridTemplateColumns = `repeat(${rowsAndCols}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rowsAndCols}, 1fr)`;
+    createSquares(rowsAndCols, container);
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "yellow";
+        });
+    }
+)
+});
